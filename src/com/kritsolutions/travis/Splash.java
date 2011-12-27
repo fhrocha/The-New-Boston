f441e81528ff1c00c58a538b96be114c7e229fa6
unknown <Fabio@.(none)>
2011-12-26 22:34:08 -0200

    Initial Commit

---
+++ src/com/kritsolutions/travis/Splash.java
@@ -0,0 +1,41 @@
+package com.kritsolutions.travis;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+	
+	MediaPlayer ourSong;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		this.ourSong = MediaPlayer.create(Splash.this, R.raw.splashsound);
+		this.ourSong.start();
+		Thread timer = new Thread() {
+			@Override
+			public void run() {
+				try {
+					sleep(14000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					Intent openStartingPoint = new Intent("com.kritsolutions.travis.STARTINGPOINT");
+					startActivity(openStartingPoint);
+				}
+			}
+		};
+		timer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		this.ourSong.release();
+		finish();
+	}
+}
