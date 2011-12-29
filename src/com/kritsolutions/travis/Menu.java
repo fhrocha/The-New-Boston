db103b38369ac493bd76c48bc164b45bd0788151
Fabio Rocha <fhrocha@gmail.com>
2011-12-28 23:28:44 -0200

    TextPlay activity

--- src/com/kritsolutions/travis/Menu.java
+++ src/com/kritsolutions/travis/Menu.java
@@ -9,7 +9,7 @@ import android.widget.ListView;
 
 public class Menu extends ListActivity {
 	
-	String[] classes = {"startingPoint", "example1"};
+	String[] classes = {"startingPoint", "TextPlay"};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
---
+++ src/com/kritsolutions/travis/TextPlay.java
@@ -0,0 +1,37 @@
+package com.kritsolutions.travis;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class TextPlay extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.text);
+		
+		Button chkCmd = (Button) findViewById(R.id.bResults);
+		final ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
+		final EditText input = (EditText) findViewById(R.id.etCommands);
+		TextView display = (TextView) findViewById(R.id.tvDisplay);
+		
+		passTog.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				if(passTog.isChecked()) {
+					input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+				} else {
+					input.setInputType(InputType.TYPE_CLASS_TEXT);
+				}
+			}
+		});
+	}
+
+	
+	
+}
