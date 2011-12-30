ebbe5314f0a2bbcc993548f80754752464d0fc69
Fabio Rocha <fhrocha@gmail.com>
2011-12-29 22:36:58 -0200

    Refactory

--- src/com/kritsolutions/travis/TextPlay.java
+++ src/com/kritsolutions/travis/TextPlay.java
@@ -15,9 +15,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-public class TextPlay extends Activity {
+public class TextPlay extends Activity implements View.OnClickListener {
 
-	Button chkCmd;
+	Button bResults;
 	ToggleButton passTog;
 	EditText input;
 	TextView display;
@@ -28,61 +28,67 @@ public class TextPlay extends Activity {
 		setContentView(R.layout.text);
 		
 		bindXMLComponents();
-		
-		passTog.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				if(passTog.isChecked()) {
-					input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-				} else {
-					input.setInputType(InputType.TYPE_CLASS_TEXT);
-				}
-			}
-		});
-		
-		chkCmd.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View v) {
-				String check = input.getText().toString();
-				display.setText(check);
-				System.out.println(display.getTextSize());
-				if(check.contentEquals("left")) {
-					display.setGravity(Gravity.LEFT);
-				} else if(check.contentEquals("center")) {
-					display.setGravity(Gravity.CENTER);
-				} else if(check.contentEquals("right")) {
-					display.setGravity(Gravity.RIGHT);
-				} else if(check.contentEquals("blue")) {
-				 	display.setTextColor(Color.BLUE);
-				} else if(check.contentEquals("WTF")) {
-					Random crazy = new Random();
-					display.setText("WTF!!!");
-					display.setTextSize(crazy.nextInt(75));
-					display.setTextColor(Color.rgb(crazy.nextInt(265), crazy.nextInt(265), crazy.nextInt(265)));
-					switch(crazy.nextInt(3)) {
-					case 0 : 
-						display.setGravity(Gravity.LEFT);
-						break;
-					case 1 :
-						display.setGravity(Gravity.CENTER);
-						break;
-					case 2 :
-						display.setGravity(Gravity.RIGHT);
-						break;
-					}
-				} else {
-					display.setText("invalid");
-					display.setTextSize(21.0f);
-					display.setGravity(Gravity.CENTER);
-					display.setTextColor(Color.WHITE);
-				}
-			}
-		});
+		passTog.setOnClickListener(this);
+		bResults.setOnClickListener(this);
 	}
 
 	private void bindXMLComponents() {
-		chkCmd = (Button) findViewById(R.id.bResults);
+		bResults = (Button) findViewById(R.id.bResults);
 		passTog = (ToggleButton) findViewById(R.id.tbPassword);
 		input = (EditText) findViewById(R.id.etCommands);
 		display = (TextView) findViewById(R.id.tvDisplay);
+	}
+
+	public void onClick(View view) {
+		
+		switch (view.getId()) {
+		
+		case R.id.bResults:
+			String check = input.getText().toString();
+			display.setText(check);
+			if(check.contentEquals("left")) {
+				display.setGravity(Gravity.LEFT);
+			} else if(check.contentEquals("center")) {
+				display.setGravity(Gravity.CENTER);
+			} else if(check.contentEquals("right")) {
+				display.setGravity(Gravity.RIGHT);
+			} else if(check.contentEquals("blue")) {
+			 	display.setTextColor(Color.BLUE);
+			} else if(check.contentEquals("WTF")) {
+				Random crazy = new Random();
+				display.setText("WTF!!!");
+				display.setTextSize(crazy.nextInt(75));
+				display.setTextColor(Color.rgb(crazy.nextInt(265), crazy.nextInt(265), crazy.nextInt(265)));
+				switch(crazy.nextInt(3)) {
+				case 0 : 
+					display.setGravity(Gravity.LEFT);
+					break;
+				case 1 :
+					display.setGravity(Gravity.CENTER);
+					break;
+				case 2 :
+					display.setGravity(Gravity.RIGHT);
+					break;
+				}
+			} else {
+				display.setText("invalid");
+				display.setTextSize(21.0f);
+				display.setGravity(Gravity.CENTER);
+				display.setTextColor(Color.WHITE);
+			}
+			break;
+			
+		case R.id.tbPassword:
+			if(passTog.isChecked()) {
+				input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+			} else {
+				input.setInputType(InputType.TYPE_CLASS_TEXT);
+			}
+			break;
+
+		default:
+			break;
+		}
+		
 	}
 }
