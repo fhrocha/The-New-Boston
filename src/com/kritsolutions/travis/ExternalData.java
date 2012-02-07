ea4a4be36ddcb353d856101dde2d116f0d4745f9
Fabio Rocha <fhrocha@gmail.com>
2012-02-06 22:06:24 -0200

    External Storage. Tutorial 108 - 111.
    Signed-off-by: Fabio Rocha <fhrocha@gmail.com>

--- src/com/kritsolutions/travis/ExternalData.java
+++ src/com/kritsolutions/travis/ExternalData.java
@@ -1,8 +1,15 @@
 package com.kritsolutions.travis;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import android.app.Activity;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -14,6 +21,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ExternalData extends Activity implements OnItemSelectedListener, OnClickListener {
 	
@@ -23,6 +31,7 @@ public class ExternalData extends Activity implements OnItemSelectedListener, On
 	private Spinner spinner;
 	private String[] paths = {"Music", "Pictures", "Downloads"};
 	private File path = null;
+	private File file = null;
 	private EditText saveFile;
 	private Button confirm, save;
 	
@@ -34,9 +43,19 @@ public class ExternalData extends Activity implements OnItemSelectedListener, On
 		canRead = (TextView) findViewById(R.id.tvCanRead);
 		confirm = (Button) findViewById(R.id.bConfirmSave);
 		save = (Button) findViewById(R.id.bSaveFile);
+		saveFile = (EditText) findViewById(R.id.etSaveAs);
 		confirm.setOnClickListener(this);
 		save.setOnClickListener(this);
+		
+		checkState();
 
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(ExternalData.this, android.R.layout.simple_spinner_item, paths);
+		spinner = (Spinner) findViewById(R.id.spinner1);
+		spinner.setAdapter(adapter);
+		spinner.setOnItemSelectedListener(this);
+	}
+
+	private void checkState() {
 		state = Environment.getExternalStorageState();
 		if(state.equals(Environment.MEDIA_MOUNTED)) {
 			canWrite.setText("true");
@@ -53,12 +72,7 @@ public class ExternalData extends Activity implements OnItemSelectedListener, On
 			canRead.setText("false");
 			canW = false;
 			canR = false;
-		}
-		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(ExternalData.this, android.R.layout.simple_spinner_item, paths);
-		spinner = (Spinner) findViewById(R.id.spinner1);
-		spinner.setAdapter(adapter);
-		spinner.setOnItemSelectedListener(this);
+		}		
 	}
 
 	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
@@ -85,7 +99,41 @@ public class ExternalData extends Activity implements OnItemSelectedListener, On
 	public void onClick(View arg0) {
 		switch(arg0.getId()) {
 			case R.id.bSaveFile:
-				
+				String f = saveFile.getText().toString();
+				file = new File(path, f + ".png");
+				checkState();
+				if(canW == canR == true) {
+					
+					path.mkdirs();
+					
+					try {
+						InputStream is = getResources().openRawResource(R.drawable.dot_mac_logo);
+						OutputStream os = new FileOutputStream(file);
+						byte[] data = new byte[is.available()];
+						is.read(data);
+						os.write(data);
+						is.close();
+						os.close();
+						
+						Toast t = Toast.makeText(ExternalData.this, "File has been Saved", Toast.LENGTH_LONG);
+						t.show();
+						
+						MediaScannerConnection.scanFile(ExternalData.this,
+														new String[]{file.toString()}, 
+														null, 
+														new MediaScannerConnection.OnScanCompletedListener() {
+															
+															public void onScanCompleted(String path, Uri uri) {
+																Toast t = Toast.makeText(ExternalData.this, "Scan Completed", Toast.LENGTH_LONG);
+																t.show();
+															}
+														});
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
 				break;
 			case R.id.bConfirmSave:
 				save.setVisibility(View.VISIBLE);
